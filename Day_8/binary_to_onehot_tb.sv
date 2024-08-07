877ba0a5640868a30bd54df0aa204684ad7bbffe
Siddharth Rana <siddharthrana95@gmail.com>
2024-08-07 16:37:28 -0400

    Update binary_to_onehot_tb.sv

--- Day_8/binary_to_onehot_tb.sv
+++ Day_8/binary_to_onehot_tb.sv
@@ -9,7 +9,7 @@ logic [BIN_W-1:0] bin_i;
 logic [ONE_HOT_W-1:0] one_hot_o;
 
 day8 #(BIN_W, ONE_HOT_W) day8_bin_onehot(.bin_i(bin_i),
-									.one_hot_o(one_hot_o));
+						.one_hot_o(one_hot_o));
 
 initial begin
     for(int i=0; i<32; i=i+1) begin
