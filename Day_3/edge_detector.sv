eba91fdc111d87d4610d4e00f2a4ee5e4c5045f1
Siddharth Rana <siddharthrana95@gmail.com>
2024-08-07 16:01:23 -0400

    Update edge_detector.sv

--- Day_3/edge_detector.sv
+++ Day_3/edge_detector.sv
@@ -1,24 +1,24 @@
 // Edge Detector and a counter to count number of edges
 
 module day3 (input wire clk,
-	input wire reset,
-	input wire a_i,
-	output wire rising_edge);
+		input wire reset,
+		input wire a_i,
+		output wire rising_edge);
 
 logic a_prev;
 int count;			//Counts number of edges
   
 always @(posedge clk) begin
-  if (reset) begin 
+	if (reset) begin 
 		a_prev <= 0;
-  end 
-  else begin
+	end 
+	else begin
     //counter logic to count the number of edges
-    if (rising_edge) begin
-      count <= count+1;
-    end
-    a_prev <= a_i;
-  end
+    	if (rising_edge) begin
+    		count <= count+1;
+    	end
+    	a_prev <= a_i;
+	end
 end
 
 // Rising edge when previous value = 0 and current value = 1
