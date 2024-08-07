51a576b7e00e98e98259c19a3d2d76e145901527
Siddharth Rana <siddharthrana95@gmail.com>
2024-08-07 15:05:32 -0400

    Update d_ff_tb.sv

--- Day_2/d_ff_tb.sv
+++ Day_2/d_ff_tb.sv
@@ -7,46 +7,47 @@ module day2_tb ();
 	logic d_i;
 	logic q;
 
-	day2 Day2_d_ff (.clk(clk),
-					.reset(reset),
-					.d_i(d_i),
-					.q(q));
+day2 Day2_d_ff (.clk(clk),
+				.reset(reset),
+				.d_i(d_i),
+				.q(q));
 
-	// Generate clk
-	initial begin
-		clk = 1'b1;
-		forever #5 clk = ~clk;
-	end
+// Generate clk
+initial begin
+	clk = 1'b1;
+	forever #5 clk = ~clk;
+end
 
-	 // Stimulus
-	initial begin
-    	reset = 1'b1;
-    	d_i = 1'b0;
-    	@(posedge clk);
-    	reset = 1'b0;
-    	#8;
-    	d_i = 1'b1;
-    	@(posedge clk);
-    	@(posedge clk);
-    	d_i = 1'b0;
-    	@(posedge clk);
-    	@(posedge clk);
-    	@(posedge clk);
-    	d_i = 1'b1;
-    	@(posedge clk);
-    	@(posedge clk);
-    	d_i = 1'b0;
-    	@(posedge clk);
-    	@(posedge clk);
-    	@(posedge clk);
-    	d_i = 1'b0;
-    	@(posedge clk);
-   		d_i = 1'b0;
-    	$finish();
-    end
+// Stimulus
+initial begin
+    reset = 1'b1;
+    d_i = 1'b0;
+    @(posedge clk);
+    reset = 1'b0;
+    #8;
+    d_i = 1'b1;
+    @(posedge clk);
+    @(posedge clk);
+    d_i = 1'b0;
+    @(posedge clk);
+    @(posedge clk);
+    @(posedge clk);
+    d_i = 1'b1;
+    @(posedge clk);
+    @(posedge clk);
+    d_i = 1'b0;
+    @(posedge clk);
+    @(posedge clk);
+    @(posedge clk);
+    d_i = 1'b0;
+    @(posedge clk);
+   	d_i = 1'b0;
+    $finish();
+end
   
-    initial begin
-    	$dumpfile("dump.vcd"); $dumpvars;
- 	end
+initial begin
+    $dumpfile("dump.vcd"); 
+    $dumpvars;
+ end
 
 endmodule
